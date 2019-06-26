c32e638d13ae8ebd3a159155df038e46115f6951
Kasper Esbjörnsson <kasper.esbjornsson@gmail.com>
2019-06-26 20:02:32 +0100

    Directory handling helper library

---
+++ SpelSylt/SpelSylt/DirectoryHelpers.cpp
@@ -0,0 +1,28 @@
+#include "DirectoryHelpers.h"
+
+
+
+//------------------------------------------------------------------
+
+void SDirectoryHelpers::CreateDirectoryIfNotExisting(const char* InRelativePath)
+{
+	const bool IsDirectory = std::filesystem::is_directory(InRelativePath);
+	const bool Exists = std::filesystem::exists(InRelativePath);
+
+	if (!IsDirectory && !Exists)
+	{
+		std::filesystem::create_directory(InRelativePath);
+	}
+}
+
+//------------------------------------------------------------------
+
+void SDirectoryHelpers::VisitDirectory(const char* InRelativePath, const FFileIteratorPredicate& InPredicate)
+{
+	for (auto&& File : std::filesystem::directory_iterator(InRelativePath))
+	{
+		InPredicate(File);
+	}
+}
+
+//------------------------------------------------------------------
