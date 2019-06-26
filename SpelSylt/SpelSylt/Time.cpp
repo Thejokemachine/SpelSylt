2eedfa14c491fe4a6023ec476693d53d7982309c
Kasper Esbjörnsson <kasper.esbjornsson@gmail.com>
2019-06-26 19:38:40 +0100

    Add possibility to get local time from CTime

--- SpelSylt/SpelSylt/Time.cpp
+++ SpelSylt/SpelSylt/Time.cpp
@@ -1,5 +1,6 @@
 #include "Time.h"
-
+#include <ctime>
+#include <iomanip>
 
 CTime::CTime()
 {
@@ -39,4 +40,18 @@ float CTime::GetDeltaTime()
 float CTime::GetTotalTime()
 {
 	return myTotalTime.count();
+}
+
+void CTime::GetNowAsDateTime(SDateTime& OutDateTime) const
+{
+	std::time_t TimeTNow = std::time(0);
+	std::tm LocalTime;// = std::localtime(&TimeTNow);
+	localtime_s(&LocalTime, &TimeTNow);
+
+	OutDateTime.Day = LocalTime.tm_mday;
+	OutDateTime.Month = LocalTime.tm_mon + 1; //Compensate for the fact that this is "months SINCE january"
+	OutDateTime.Year = LocalTime.tm_year + 1900; //tm_year returns years SINCE 1900 so to get current year we add 1900
+	OutDateTime.Hour = LocalTime.tm_hour;
+	OutDateTime.Minute = LocalTime.tm_min;
+	OutDateTime.Second = LocalTime.tm_sec;
 }
--- SpelSylt/SpelSylt/Time.h
+++ SpelSylt/SpelSylt/Time.h
@@ -1,5 +1,35 @@
 #pragma once
 #include <chrono>
+#include <string>
+
+struct SDateTime
+{
+	unsigned char Day;
+	unsigned char Month;
+	unsigned short Year;
+	unsigned char Hour;
+	unsigned char Minute;
+	unsigned char Second;
+
+	void ToString(std::string& OutString)
+	{
+		//Make date
+		OutString = std::to_string(Year);
+		OutString += "-";
+		OutString += std::to_string(Month);
+		OutString += "-";
+		OutString += std::to_string(Day);
+
+		OutString += " ";
+
+		OutString += std::to_string(Hour);
+		OutString += "h";
+		OutString += std::to_string(Minute);
+		OutString += "m";
+		OutString += std::to_string(Second);
+		OutString += "s";
+	}
+};
 
 class CTime
 {
@@ -13,6 +43,8 @@ public:
 
 	float GetDeltaTime();
 	float GetTotalTime();
+
+	void GetNowAsDateTime(SDateTime& OutDateTime) const;
 
 	struct STimeData
 	{
