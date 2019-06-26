afe48d1094619845289c61a30d975c070268ec98
Kasper Esbjörnsson <kasper.esbjornsson@gmail.com>
2019-06-26 20:06:15 +0100

    Remove blank lines in DirectoryHelpers.cpp

--- SpelSylt/SpelSylt/DirectoryHelpers.cpp
+++ SpelSylt/SpelSylt/DirectoryHelpers.cpp
@@ -1,7 +1,5 @@
 #include "DirectoryHelpers.h"
 
-
-
 //------------------------------------------------------------------
 
 void SDirectoryHelpers::CreateDirectoryIfNotExisting(const char* InRelativePath)
