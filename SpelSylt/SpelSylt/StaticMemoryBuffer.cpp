c6a266b0993ac761655faa2ad9ac059e10001244
Kasper Esbjörnsson <kasper.esbjornsson@gmail.com>
2019-06-30 01:55:37 +0100

    Added StaticMemoryBuffer for storing raw data

---
+++ SpelSylt/SpelSylt/StaticMemoryBuffer.cpp
@@ -0,0 +1,61 @@
+#include "StaticMemoryBuffer.h"
+
+//------------------------------------------------------------------
+
+CStaticMemoryBuffer::CStaticMemoryBuffer()
+	: NextSlot(0)
+	, Size(0)
+	, Buffer(nullptr)
+{
+}
+
+//------------------------------------------------------------------
+
+CStaticMemoryBuffer::CStaticMemoryBuffer(unsigned short InSize)
+	: NextSlot(0)
+	, Size(0)
+	, Buffer(nullptr)
+{
+	Allocate(InSize);
+}
+
+//------------------------------------------------------------------
+
+CStaticMemoryBuffer::~CStaticMemoryBuffer()
+{
+	delete[Size] Buffer;
+}
+
+//------------------------------------------------------------------
+
+void CStaticMemoryBuffer::Allocate(unsigned short InSize)
+{
+	Size = InSize;
+	Buffer = new RawByte[Size];
+}
+
+//------------------------------------------------------------------
+
+unsigned short CStaticMemoryBuffer::GetRemainingSize() const
+{
+	return Size - NextSlot;
+}
+
+//------------------------------------------------------------------
+
+void CStaticMemoryBuffer::Flush()
+{
+	NextSlot = 0;
+}
+
+//------------------------------------------------------------------
+
+void* CStaticMemoryBuffer::AddCustomData(const void* InStart, unsigned short InSize)
+{
+	memcpy(&Buffer[NextSlot], InStart, InSize);
+	void* Added = reinterpret_cast<void*>(&Buffer[NextSlot]);
+	NextSlot += InSize;
+	return Added;
+}
+
+//------------------------------------------------------------------
---
+++ SpelSylt/SpelSylt/StaticMemoryBuffer.h
@@ -0,0 +1,56 @@
+#pragma once
+#include <cstring>
+
+typedef unsigned char RawByte;
+
+class CStaticMemoryBuffer
+{
+public:
+	CStaticMemoryBuffer();
+	CStaticMemoryBuffer(unsigned short InSize);
+	~CStaticMemoryBuffer();
+
+	//Removed constructors
+	CStaticMemoryBuffer(const CStaticMemoryBuffer& InStaticMemoryBuffer) = delete;
+	CStaticMemoryBuffer(CStaticMemoryBuffer&& InStaticMemoryBuffer) = delete;
+
+	void Allocate(unsigned short InSize);
+	unsigned short GetRemainingSize() const;
+	void Flush();
+
+	void* AddCustomData(const void* InStart, unsigned short InSize);
+
+	template<typename T>
+	bool CanAddType() const;
+
+	template<typename T>
+	T& AddToBuffer(const T& InData);
+
+private:
+	unsigned short NextSlot;
+	unsigned short Size;
+	RawByte* Buffer;
+};
+
+//------------------------------------------------------------------
+
+template<typename T>
+inline bool CStaticMemoryBuffer::CanAddType() const
+{
+	return sizeof(T) <= GetRemainingSize();
+}
+
+//------------------------------------------------------------------
+
+template<typename T>
+inline T& CStaticMemoryBuffer::AddToBuffer(const T& InData)
+{
+	memcpy(&Buffer[NextSlot], &InData, sizeof(T));
+	T* Added = reinterpret_cast<T*>(&Buffer[NextSlot]);
+	NextSlot += sizeof(T);
+
+	return *Added;
+}
+
+//------------------------------------------------------------------
+
