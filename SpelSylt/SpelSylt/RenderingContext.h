ef84dc2d3f3554b1a975142e5506ff20af0f0f39
Kasper Esbjörnsson <kasper.esbjornsson@gmail.com>
2019-06-30 14:16:33 +0100

    Add IDebugDrawerInput to the RenderingContext

--- SpelSylt/SpelSylt/RenderingContext.h
+++ SpelSylt/SpelSylt/RenderingContext.h
@@ -2,16 +2,22 @@
 
 #include "RenderCommand.h"
 #include "RenderQueueInput.h"
+#include "DebugDrawerInput.h"
 
 struct SRenderingContext
 {
 public:
+	SRenderingContext() = delete;
+
 	SRenderingContext(
-		IRenderQueueInput& InRenderQueueInput
+		IRenderQueueInput& InRenderQueueInput,
+		IDebugDrawerInput& InDebugDrawerInput 
 	)
 		: RenderQueue(InRenderQueueInput)
+		, DebugDrawer(InDebugDrawerInput)
 	{
 	};
 
 	IRenderQueueInput& RenderQueue;
+	IDebugDrawerInput& DebugDrawer;
 };
