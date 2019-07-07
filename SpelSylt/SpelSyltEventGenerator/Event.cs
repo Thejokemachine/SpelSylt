c45759f684815e01a613bcbcf554d19ee37b5223
Kasper Esbjörnsson <kasper.esbjornsson@gmail.com>
2019-07-07 02:40:41 +0100

    Initial test for Event Generator

---
+++ SpelSylt/SpelSyltEventGenerator/Event.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpelSyltEventGenerator
+{
+    class Event
+    {
+        public string EventName;
+        public List<string> ParamTypes = new List<string>();
+    }
+}
---
+++ SpelSylt/SpelSyltEventGenerator/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpelSyltEventGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ReadFile = args[0];
+            string OutFile = args[1];
+
+            List<Event> EventList = new List<Event>();
+
+            EventReader Reader = new EventReader();
+            Reader.ReadEvents(ReadFile, ref EventList);
+
+            EventDuplicateRemover DuplicateRemover = new EventDuplicateRemover();
+            DuplicateRemover.RemoveAllDuplicates(ref EventList);
+
+            EventParser Parser = new EventParser();
+            Parser.ParseEvents(ref EventList, OutFile);
+        }
+    }
+}
