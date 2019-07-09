5920087201dc734b0805d3cf009dbdacdf87126d
Kasper Esbjörnsson <kasper.esbjornsson@gmail.com>
2019-07-09 21:50:23 +0100

    Added SpelSyltMinmal header that includes all basics

---
+++ SpelSylt/SpelSylt/SpelSyltMinimal.h
@@ -0,0 +1,36 @@
+#pragma once
+
+//Logging
+#include <SpelSylt/Debugging/Logging/DebugLogger.h>
+
+//Messaging
+#include <SpelSylt/Messaging/Messages/BaseMessage.h>
+#include <SpelSylt/Messaging/MessageQueue.h>
+
+//Contexts
+#include <SpelSylt/Contexts/GameContext.h>
+#include <SpelSylt/Contexts/RenderingContext.h>
+
+//Rendering
+#include <SpelSylt/Rendering/RenderCommand.h>
+#include <SpelSylt/Rendering/RenderQueueInput.h>
+#include <SpelSylt/Debugging/Rendering/DebugDrawerInput.h>
+
+//File handling
+#include <SpelSylt/FileHandling/Loading/AsyncLoaderInterface.h>
+
+//Banks
+#include <SpelSylt/FileHandling/Banks/TextureProviderInterface.h>
+#include <SpelSylt/FileHandling/Banks/FontBank.h>
+
+//Utility
+#include <SpelSylt/Utility/Time/TimeGetter.h>
+#include <SpelSylt/Utility/Input/InputEventGetter.h>
+
+//Math
+#include <SpelSylt/Math/CommonMath.h>
+
+//SFML
+#include <SFML/System/Vector2.hpp>
+#include <SFML/Graphics/Rect.hpp>
+#include <SFML/Graphics/View.hpp>
