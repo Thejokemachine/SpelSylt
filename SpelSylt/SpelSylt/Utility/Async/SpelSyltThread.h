6b5f3fd931b2a5373c2f1ab52411806d26a46471
Kasper Esbjörnsson <kasper.esbjornsson@gmail.com>
2019-07-14 18:21:27 +0100

    Emplacing Worker to thread now returns ref to emplaced worker

--- SpelSylt/SpelSylt/Utility/Async/SpelSyltThread.h
+++ SpelSylt/SpelSylt/Utility/Async/SpelSyltThread.h
@@ -16,7 +16,7 @@ namespace SpelSylt
 		CSSThread(const CSSThread&) = delete;
 
 		template<typename TWorkerType, typename ...TEmplaceArgs>
-		void EmplaceWorker(TEmplaceArgs...);
+		TWorkerType& EmplaceWorker(TEmplaceArgs...);
 
 		void Start();
 		void RequestStop();
@@ -40,8 +40,9 @@ namespace SpelSylt
 namespace SS = SpelSylt;
 
 template<typename TWorkerType, typename ...TEmplaceArgs>
-inline void SpelSylt::CSSThread::EmplaceWorker(TEmplaceArgs ... Args)
+inline TWorkerType& SpelSylt::CSSThread::EmplaceWorker(TEmplaceArgs ... Args)
 {
 	TWorkerType& Worker = WorkerStorage.AddToBuffer<TWorkerType>(TWorkerType(Args...));
 	WorkerList.push_back(&Worker);
+	return Worker;
 }
