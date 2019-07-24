4ecf93edf4e8425a190d49deb264ffdaf7c4d920
Kasper Esbjörnsson <kasper.esbjornsson@gmail.com>
2019-07-24 20:00:29 +0100

    Moved Texture asset into AssetTypes sub dir

---
+++ SpelSylt/SpelSylt/FileHandling/Asset/AssetTypes/TextureAsset.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "SpelSylt/FileHandling/Asset/Asset.h"
+
+#include <SFML/Graphics/Texture.hpp>
+
+namespace SpelSylt
+{
+	struct STextureAsset
+		: public SBaseAsset
+		, public sf::Texture
+	{
+		STextureAsset()
+			: SBaseAsset()
+			, sf::Texture()
+		{
+		}
+
+		sf::Vector2u GetSize() const
+		{
+			if (LoadStatus != ELoadRequestStatus::Done)
+			{
+				return sf::Vector2u(64, 64);
+			}
+
+			return getSize();
+		}
+
+	protected:
+		virtual void AssignData(char* InDataLocation, B InDataSize) override
+		{
+			loadFromMemory(InDataLocation, InDataSize);
+		}
+	};
+}
+
+namespace SS = SpelSylt;
