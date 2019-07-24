3a04726009fa30878fd042b1812da63183a5a7ae
Kasper Esbjörnsson <kasper.esbjornsson@gmail.com>
2019-07-24 20:04:43 +0100

    Delete setTexture function from CSprite (use SetTextureAsset() )

--- SpelSylt/SpelSylt/Rendering/Sprite/Sprite.h
+++ SpelSylt/SpelSylt/Rendering/Sprite/Sprite.h
@@ -1,9 +1,5 @@
 #pragma once
-#include <SFML/Graphics/Transformable.hpp>
-#include <SFML/Graphics/Drawable.hpp>
 #include <SFML/Graphics/Sprite.hpp>
-
-struct STextureAsset;
 
 namespace sf
 {
@@ -14,11 +10,16 @@ namespace sf
 
 namespace SpelSylt
 {
+	struct STextureAsset;
+	
 	class CSprite
 		: public sf::Sprite
 	{
 	public:
 		CSprite();
+
+		//This function is deleted due to SS::CSprite working with SS::STextureAssets instead
+		void setTexture(const sf::Texture& texture, bool resetRect) = delete;
 
 		void SetTextureAsset(const STextureAsset& Asset);
 		virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
