4f36913f179fec6879dffec58420040070938495
Kasper Esbjörnsson <kasper.esbjornsson@gmail.com>
2019-07-24 20:04:11 +0100

    Asset provider now returning references instead of ptrs

--- SpelSylt/SpelSylt/FileHandling/Asset/AssetProvider.h
+++ SpelSylt/SpelSylt/FileHandling/Asset/AssetProvider.h
@@ -16,8 +16,7 @@ namespace SpelSylt
 
 	public:
 		virtual void ProvideLoader(IAsyncLoader& InLoader) = 0;
-	private:
-		virtual SBaseAsset* GetAsset(const char* InAssetPath) = 0;
+		virtual SBaseAsset& GetAsset(const char* InAssetPath) = 0;
 	};
 }
 
