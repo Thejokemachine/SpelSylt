49281b9aba43e28e0285440ac4281fdf7ff0c2ec
Kasper Esbjörnsson <kasper.esbjornsson@gmail.com>
2019-07-28 22:29:20 +0100

    Loading State now have a spinning killer whale

--- SpelSylt/Game/LoadingState.h
+++ SpelSylt/Game/LoadingState.h
@@ -2,6 +2,9 @@
 #include <SpelSylt/UI/Base/UIState.h>
 #include <SpelSylt/Utility/TimedEvent.h>
 
+#include <SpelSylt/Rendering/Sprite/Sprite.h>
+#include <SpelSylt/Rendering/Sprite/Texture.h>
+
 class LoadingState : public UI::UIState
 {
 public:
@@ -10,8 +13,10 @@ public:
 	virtual void OnInit(SGameContext& InGameContext) override;
 	virtual void OnUpdate(SGameContext& InGameContext) override;
 	virtual void OnRender(sf::RenderTarget& InTarget) override;
-
 private:
+	float Rotation;
+	SS::CTexture Texture;
+	SS::CSprite Sprite;
 
 	CTimedEvent myLoadingTimer;
 };
