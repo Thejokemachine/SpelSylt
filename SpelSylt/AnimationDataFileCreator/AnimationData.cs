0f615f032fb53b0b93584213feb7177a86142f18
Kasper Esbjörnsson <kasper.esbjornsson@gmail.com>
2019-08-01 23:14:32 +0100

    Removed AnimationDataFileCreator project from ignore

---
+++ SpelSylt/AnimationDataFileCreator/AnimationData.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace AnimationDataFileCreator
+{
+    struct AnimationFrame
+    {
+        public float time;
+        public int x,y,w,h;
+    }
+
+    struct AnimationData
+    {
+        public List<AnimationFrame> frames;
+    }
+
+    static class AnimationDataJsonParser
+    {
+        public static void Parse(string InFullPath, AnimationData InDataToWrite)
+        {
+            StreamWriter WriterText = new StreamWriter(InFullPath, false);
+            JsonSerializer Serializer = new JsonSerializer();
+            string JsonData = JsonConvert.SerializeObject(InDataToWrite);
+            WriterText.Write(JsonData);
+            WriterText.Close();
+        }
+    }
+}
---
+++ SpelSylt/AnimationDataFileCreator/DirectoryHelper.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.WindowsAPICodePack.Dialogs;
+
+namespace AnimationDataFileCreator
+{
+    static class DirectoryHelper
+    {
+        public static bool PromptForDirectory(ref string OutDirPath)
+        {
+            CommonOpenFileDialog DirectoryPicker = new CommonOpenFileDialog();
+            DirectoryPicker.DefaultDirectory = Directory.GetCurrentDirectory();
+            DirectoryPicker.IsFolderPicker = true;
+
+            if(DirectoryPicker.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                OutDirPath = DirectoryPicker.FileName;
+                OutDirPath += "\\";
+                return true;
+            }
+
+            return false;
+        }
+
+        public static List<string> GetAllFilesWithExtension(string InPath, string InExtension)
+        {
+            List<string> CollectedList = new List<string>();
+
+            string[] AllFiles = Directory.GetFiles(InPath);
+
+            foreach(string FilePath in AllFiles )
+            {
+                if(Path.GetExtension(FilePath) == InExtension)
+                {
+                    string FileName = Path.GetFileName(FilePath);
+                    CollectedList.Add(FileName);
+                }
+            }
+
+            return CollectedList;
+        }
+
+        public static string GetFileNameNoExtension(string InFile)
+        {
+            return Path.GetFileNameWithoutExtension(InFile);
+        }
+    }
+}
