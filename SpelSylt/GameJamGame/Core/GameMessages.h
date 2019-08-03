4ee5971331615d024dea49da0316dda201812706
Kasper Esbjörnsson <kasper.esbjornsson@gmail.com>
2019-08-03 17:44:54 +0100

    Added hitscan weapon shot message

--- SpelSylt/GameJamGame/Core/GameMessages.h
+++ SpelSylt/GameJamGame/Core/GameMessages.h
@@ -1,16 +1,23 @@
 #pragma once
 #include <SpelSylt/Messaging/Messages/BaseMessage.h>
 #include <SFML/System/Vector2.hpp>
+#include <utility>
 
 DEFINE_MSG(InteractMsg)
 
 // Water messages
 DEFINE_MSG(PickedUpWater)
 DEFINE_MSG(WaterMsg)
-DEFINE_MSG(TreeAttackedMsg)
 DEFINE_MSG(ShowWaterPrompt)
 DEFINE_MSG(HideWaterPrompt)
 
 // Weapon messages
 DEFINE_MSG_ONEPARAM(FireWeaponMsg, sf::Vector2f)
 DEFINE_MSG_ONEPARAM(SwitchWeaponMsg, int)
+
+using FHitscanMessageParam = std::pair<sf::Vector2f, sf::Vector2f>;
+DEFINE_MSG_ONEPARAM(HitscanShotMsg, FHitscanMessageParam)
+
+
+//Enemy messages
+DEFINE_MSG(TreeAttackedMsg)
