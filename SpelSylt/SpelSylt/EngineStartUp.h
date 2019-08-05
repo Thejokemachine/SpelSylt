889739d18b91d847abac0512d17664eb8d2f49ca
Unknown <joakimbensryd@gmail.com>
2019-08-05 22:03:37 +0200

    Changed paths to libs.

--- SpelSylt/SpelSylt/EngineStartUp.h
+++ SpelSylt/SpelSylt/EngineStartUp.h
@@ -3,9 +3,9 @@
 #include "SpelSylt/sfml_libs.h"
 
 #ifdef _DEBUG
-#pragma comment(lib, "../External/Engine/SpelSylt_Debug")
-#pragma comment(lib, "../External/RapidJson_Debug")
+#pragma comment(lib, "SpelSylt_Debug")
+#pragma comment(lib, "RapidJson_Debug")
 #else
-#pragma comment(lib, "../External/Engine/SpelSylt_Release")
-#pragma comment(lib, "../External/RapidJson_Release")
+#pragma comment(lib, "SpelSylt_Release")
+#pragma comment(lib, "RapidJson_Release")
 #endif
