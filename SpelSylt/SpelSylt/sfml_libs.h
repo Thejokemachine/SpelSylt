30fb5138444ab5970ae2276ade426ed97cc7a56f
Kasper Esbjörnsson <kasper.esbjornsson@gmail.com>
2019-08-07 23:22:27 +0100

    SFML Libs now respect dir structure

--- SpelSylt/SpelSylt/sfml_libs.h
+++ SpelSylt/SpelSylt/sfml_libs.h
@@ -2,17 +2,17 @@
 
 
 #ifdef _DEBUG
-#pragma comment(lib, "sfml-audio-d.lib")
-#pragma comment(lib, "sfml-graphics-d.lib")
-#pragma comment(lib, "sfml-main-d.lib")
-#pragma comment(lib, "sfml-network-d.lib")
-#pragma comment(lib, "sfml-system-d.lib")
-#pragma comment(lib, "sfml-window-d.lib")
+#pragma comment(lib, "SFML/sfml-audio-d.lib")
+#pragma comment(lib, "SFML/sfml-graphics-d.lib")
+#pragma comment(lib, "SFML/sfml-main-d.lib")
+#pragma comment(lib, "SFML/sfml-network-d.lib")
+#pragma comment(lib, "SFML/sfml-system-d.lib")
+#pragma comment(lib, "SFML/sfml-window-d.lib")
 #else
-#pragma comment(lib, "sfml-audio.lib")
-#pragma comment(lib, "sfml-graphics.lib")
-#pragma comment(lib, "sfml-main.lib")
-#pragma comment(lib, "sfml-network.lib")
-#pragma comment(lib, "sfml-system.lib")
-#pragma comment(lib, "sfml-window.lib")
+#pragma comment(lib, "SFML/sfml-audio.lib")
+#pragma comment(lib, "SFML/sfml-graphics.lib")
+#pragma comment(lib, "SFML/sfml-main.lib")
+#pragma comment(lib, "SFML/sfml-network.lib")
+#pragma comment(lib, "SFML/sfml-system.lib")
+#pragma comment(lib, "SFML/sfml-window.lib")
 #endif
