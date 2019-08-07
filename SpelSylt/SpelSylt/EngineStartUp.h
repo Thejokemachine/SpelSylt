8308db71596154035e919474077d0855ad151455
Kasper Esbjörnsson <kasper.esbjornsson@gmail.com>
2019-08-07 23:21:29 +0100

    Engine Start Up header now respects dir structure

--- SpelSylt/SpelSylt/EngineStartUp.h
+++ SpelSylt/SpelSylt/EngineStartUp.h
@@ -3,9 +3,9 @@
 #include "SpelSylt/sfml_libs.h"
 
 #ifdef _DEBUG
-#pragma comment(lib, "SpelSylt_Debug")
+#pragma comment(lib, "Engine/SpelSylt_Debug")
 #pragma comment(lib, "RapidJson_Debug")
 #else
-#pragma comment(lib, "SpelSylt_Release")
+#pragma comment(lib, "Engine/SpelSylt_Release")
 #pragma comment(lib, "RapidJson_Release")
 #endif
